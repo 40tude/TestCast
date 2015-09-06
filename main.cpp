4d6ac7eff45ab9ffed5a1a445959e76995fa63df
Philippe BAUCOUR <philippe.baucour@gmail.com>
2015-09-06 23:49:28 +0200

    Few edits. Try to avoid the optimizer goes to far.

--- main.cpp
+++ main.cpp
@@ -5,21 +5,25 @@ using namespace std;
 using namespace chrono;
 
 int main() {
-  auto start = high_resolution_clock::now();
   
-  constexpr int NbIte = 1000000;
-  for (auto i = 0; i != NbIte; ++i) {
-    unsigned char rhs = 'a';
-    double lhs = static_cast<double>(rhs);
-    lhs = lhs;
-  }
+  constexpr int           NbIte = 1000000;
+  constexpr unsigned char rhs = 'a';
 
-  auto end = high_resolution_clock::now();
-  auto dur = duration_cast<nanoseconds>(end - start);
-  cout << "Total time     : " << dur.count() << " ns" << endl;
-  cout << "Time per cast  : " << dur.count()/NbIte << " ns" << endl;
+  auto Start = high_resolution_clock::now();
+    double lhs;                                                                 // lhs declared outside the for loop on purpose
+    for (auto i = 0; i != NbIte; ++i) {
+      lhs = static_cast<double>(rhs);
+    }
+  auto End = high_resolution_clock::now();
+  auto Bench = duration_cast<nanoseconds>(End - Start);
+  
+  cout << lhs << endl;                                                          // make sure the optimizer really update lhs in the for loop
+                                                                                // I'm not even sure it does what I really want because this guy is pretty smart :-)
+  
+  cout << "Total time     : " << Bench.count() << " ns" << endl;
+  cout << "Time per cast  : " << Bench.count()/NbIte << " ns" << endl;
 
   cout << "Press ENTER to quit : ";
-  cin.ignore((numeric_limits<streamsize>::max)(), '\n');
+  cin.ignore((numeric_limits<streamsize>::max)(), '\n');                        // avoid getchar()
 
 }
